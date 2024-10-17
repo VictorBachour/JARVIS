b47cf8993c7ae8fc7f3a2d7e7145393e30750668
vbacho <victorbachour@gmail.com>
2024-10-16 19:24:52 -0700

    It gets into open app

--- JARVIS.py
+++ JARVIS.py
@@ -10,25 +10,23 @@ class Jarvis:
         self.wake_word_said = False
 
     def open_app(self):
-        print("Listening for words")
+        print("In open app")
 
     def main_loop(self):
-        while self.wake_word_said is False:
-            with sr.Microphone() as source:
-                print('getting rid of ambient au')
-                self.recognizer.adjust_for_ambient_noise(source)
-                text = None
+        with sr.Microphone() as source:
+            text = None
+            while(self.wake_word_said is False):
                 try:
-                    print("listening")
-                    while(text == None or text.contains(self.wake_word) == False):
-                        word = self.recognizer.listen(source)
-                        text = self.recognizer.recognize_google(word).lower()
-                    self.wake_word = True
+                    self.recognizer.adjust_for_ambient_noise(source)
+                    word = self.recognizer.listen(source)
+                    text = self.recognizer.recognize_google(word).lower()
+                    if(self.wake_word in text):
+                        self.wake_word_said = True
                 except sr.UnknownValueError:
-                    print("Sorry, I could not understand the audio.")
-                except sr.RequestError:
-                    print("Sorry, there was an issue with the speech recognition service.")
-            #get it to just always listen for words then always just add then delete words maybe?
+                    None
+            self.open_app()
+
+
 
 jarvis = Jarvis()
 jarvis.main_loop()
