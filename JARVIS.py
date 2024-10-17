0302c997034e09792758c4f52c25015615f1f54e
vbacho <victorbachour@gmail.com>
2024-10-16 20:15:20 -0700

    got a narrator just working on what the user input will be

--- JARVIS.py
+++ JARVIS.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import pyttsx3
 import os
 import time
-
+import pyttsx3
 class Jarvis:
     def __init__(self):
         self.wake_word = "jarvis"
@@ -19,7 +19,28 @@ class Jarvis:
     def close_app(self):
         print('In close app')
     def open_or_close_app(self):
-        print('in open or close app')
+        narrator = pyttsx3.init()
+        narrator.setProperty('rate', 150)
+        narrator.setProperty('volume', 1)
+        narrator.say("What would you like me to open or close?")
+        narrator.runAndWait()
+        nothing_was_said = True
+        with sr.Microphone() as source:
+            text = None
+        while nothing_was_said:
+            try:
+                print('getting rid of ambience')
+                self.recognizer.adjust_for_ambient_noise(source)
+                print('listening')
+                word = self.recognizer.listen(source)
+                print('translating')
+                text = self.recognizer.recognize_google(word).lower()
+                print(f'the text that was said is {text}')
+                #finish up here
+                if('open' in text and 'close' in text or 'clothes' in text):
+                    None
+            except sr.UnknownValueError:
+                None
     def main_loop(self):
         with sr.Microphone() as source:
             text = None
@@ -38,7 +59,7 @@ class Jarvis:
                         self.wake_word_said = True
                     if('open' in text and self.wake_word_said):
                         self.open_was_said = True
-                    if('close' in text and self.wake_word_said):
+                    if('close' in text or 'clothes' in text and self.wake_word_said):
                         self.close_was_said = True
                     if(self.close_was_said is False and self.open_was_said is False and self.wake_word_said is True):
                         self.open_or_close_app()
@@ -46,9 +67,10 @@ class Jarvis:
                     None
             self.open_app() if self.open_was_said else None
             self.close_app() if self.close_was_said else None
+            self.open_or_close_app() if self.open_was_said is False and self.close_was_said is False else None
 
 
 
 
 jarvis = Jarvis()
-jarvis.main_loop()
+jarvis.open_or_close_app()
